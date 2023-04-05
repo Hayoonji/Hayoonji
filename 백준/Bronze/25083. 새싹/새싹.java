5ac577353bf2669e3ee4209920acf3c9961e64ab
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-04-05 15:26:50 +0900

    [Bronze V] Title: 새싹, Time: 188 ms, Memory: 17416 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+
+        scanner.close();
+    }    
+}
