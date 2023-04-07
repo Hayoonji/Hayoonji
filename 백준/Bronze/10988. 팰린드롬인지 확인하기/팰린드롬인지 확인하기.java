4287bc73e038e21a349f20e4ed80872705635f7a
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-04-07 22:35:22 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 208 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        boolean checked = true;
+
+        for (int i = 0 ; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(str.length() -1 -i )) {
+                checked = false;
+                break;
+            }
+        }
+        if (checked == true) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
