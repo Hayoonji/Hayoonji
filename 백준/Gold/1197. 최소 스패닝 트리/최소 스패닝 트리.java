73e6377863c6ef237ac0669e6d7156361def6e5c
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-05-23 22:08:17 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 672 ms, Memory: 50152 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -11,9 +11,9 @@ class node implements Comparable<node>{
 
 	public node(int s, int e, int v) {
 		super();
-		this.s = s;
-		this.e = e;
-		this.v = v;
+		this.s = s;//노드1
+		this.e = e;//노드2
+		this.v = v;//가중치
 	}
 
 	// 간선의 크기로 오름차순하기 위한 compareTo()메서드 
@@ -27,42 +27,46 @@ class node implements Comparable<node>{
 public class Main {
 
 	static int E, V, sum;
-	static int[] parent;
-	static List<node> list = new ArrayList();
+	static int[] parent; //부모배열
+	static List<node> list = new ArrayList();//
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		E = Integer.parseInt(st.nextToken()); //간선 개수
 		V = Integer.parseInt(st.nextToken()); //정점 개수
-
+        //1. 부모 배열 만들기
 		parent = new int[E + 1];
-
 		for (int i = 0; i < parent.length; i++) {
 			parent[i] = i;
 		}
 
-        //입력 받기
+        //2. 입력 받기
 		for (int i = 0; i < V; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken()); //노드1
+			int e = Integer.parseInt(st.nextToken()); //노드2
+			int v = Integer.parseInt(st.nextToken()); //가중치
 			list.add(new node(s,e,v));
 		}
-		
-		// 1. 간선의 크기로 오름차순 정렬
+		//크루스칼 과정
+        //즉, 그래프 간선들을 가중치의 오름차순으로 정렬해 놓은 뒤, 
+        //사이클을 형성하지 않는 선에서 정렬된 순서대로 간선을 선택합니다.
+        //cf) 사이클형성하는지 아는 법:각 노드의 부모가 동일하면 그 노드들끼리 union하면안됨
+       
+		// 3. 간선 가중치의 크기로 오름차순 정렬
 		Collections.sort(list);
 		
 		int size = list.size();
-		// 2. 정렬된 순서대로 간선 탐색
+		// 4. 정렬된 순서대로 간선 탐색
 		for (int i = 0; i < size; i++) {
 			node n = list.get(i);
-			// 만약 두 노드의 부모가 다르다면
+            
+			// 5.(사이클탐색)만약 두 노드의 부모가 다르다면(부모가 같으면 두 노드 연결X)
 			if(!isSameParent(n.s, n.e)) {
 				// sum에 간선 크기 저장
 				sum+=n.v;
-				// 두 노드 연결
+				// 6. 두 노드 연결
 				union(n.s,n.e);
 			}
 			
@@ -71,7 +75,7 @@ public class Main {
 		System.out.println(sum);
 
 	}
-
+    //============union find(크루스칼 기본)==============
 	// 간선 연결
 	private static void union(int a, int b) {
 		a = find(a);
@@ -84,10 +88,12 @@ public class Main {
 
 	// 부모 노드 찾기
 	private static int find(int a) {
+        //1) 부모가 내 자신이면
 		if (parent[a] == a) {
-			return a;
+			return a; //그대로
 		}
-		return parent[a] = find(parent[a]);
+        //2) 부모가 다른애면 path compression 
+		return parent[a] = find(parent[a]); //다른 노드로 합침
 	}
 
 	// 부모가 같은지를 판별해주는 method
