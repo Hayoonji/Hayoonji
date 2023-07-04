a4bc06e52f097bd8f645158c99353688d9353c6c
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-07-04 22:46:05 +0900

    [level 2] Title: 올바른 괄호, Time: 9.10 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = True
+    
+    stack=[]
+    for i in s:
+        if i=='(':
+            stack.append(i)
+        else:
+            if len(stack) > 0:
+                stack.pop()
+            else:
+                return False
+    if len(stack)>0:
+        return False
+    else:
+        return True
+        
+
+    return True
