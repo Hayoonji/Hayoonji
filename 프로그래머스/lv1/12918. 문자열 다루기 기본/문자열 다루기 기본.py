9480e24bbf1fd3adc5404229feaaced79802642d
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-07-14 11:35:35 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,12 @@
+def solution(s):
+    
+    count=0
+    #for i in s:
+     #   if i.isdigit()==True:
+      #      count+=1
+            
+    #if count==len(s):
+     #   answer=True
+    #else:
+     #   answer=False
+    return s.isdigit() and len(s) in [4,6]
