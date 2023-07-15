788585d90014dac256c914e93c122002e09555a9
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-07-15 14:53:18 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    
+    a=s.split(" ")
+    c=" "
+    for i in range(len(a)):
+        a[i]=a[i].capitalize()
+        #c+=b[0].upper() + b[1:].lower()+" "
+        
+    d=" ".join(a)    
+    
+    return " ".join(a)    
+    
